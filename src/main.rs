20a82f7711a5a2c1e8422046cf62e4caba8ba515
Masaki Hara <ackie.h.gmai@gmail.com>
2017-11-12 18:13:00 +0900

    Implement encoding detection.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate regex;
 #[macro_use]
 extern crate clap;
 
+use std::io::{Read, BufRead, BufReader};
+use std::fs::File;
+use std::str;
 use clap::{Arg, App};
 
 fn main() {
@@ -20,5 +24,43 @@ fn main() {
              .help("Specify files to merge."))
         .get_matches();
 
-    println!("{:?}", matches);
+    let merge_filenames = matches.values_of("files").map(|a| a.collect())
+        .unwrap_or_else(|| vec!["-"]);
+    let subtract_filenames = matches.values_of("subtract_files")
+        .map(|a| a.collect()).unwrap_or_else(|| vec![]);
+    for &filename in &merge_filenames {
+        let file = File::open(filename).unwrap();
+        let mut file = BufReader::new(file);
+        let encoding = detect_encoding(&mut file).unwrap_or_else(|| "euc-jp".to_string());
+        println!("{:?}", encoding);
+    }
+    for &filename in &subtract_filenames {
+        println!("{:?}", filename);
+    }
+}
+
+fn detect_encoding<I: BufRead>(f: &mut I) -> Option<String> {
+    use regex::bytes::Regex;
+
+    let buf = f.fill_buf().unwrap();
+    if &buf[..2] == b"\xFE\xFF" {
+        return Some("utf-16be".to_string());
+    }
+    if &buf[..2] == b"\xFF\xFE" {
+        return Some("utf-16le".to_string());
+    }
+
+    let eol = buf.iter().position(|&b| b == b'\r' || b == b'\n')
+        .unwrap_or(buf.len());
+    let buf = &buf[..eol];
+
+    let re = Regex::new(r"(?-u)coding:\s*([a-zA-Z0-9_-]+)").unwrap();
+    if let Some(m) = re.captures(buf) {
+        let name = m.get(1).unwrap().as_bytes();
+        if let Ok(name) = str::from_utf8(name) {
+            return Some(name.to_string());
+        }
+    }
+
+    return None;
 }
