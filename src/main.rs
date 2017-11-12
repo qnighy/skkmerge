ac7fd8e7028a6b5c8f19d349081e2dfe9c9950ae
Masaki Hara <ackie.h.gmai@gmail.com>
2017-11-12 21:19:27 +0900

    Output merge/diff.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate encoding;
 #[macro_use]
 extern crate clap;
 
+use std::collections::HashMap;
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 use std::str;
@@ -16,6 +17,9 @@ fn main() {
         App::new("skkmerge")
         .version(crate_version!())
         .author("Masaki Hara <ackie.h.gmai@gmail.com>")
+        .arg(Arg::with_name("retain_okuri_entries")
+             .long("--retain-okuri-entries")
+             .help("Retain okuri entries like /[け /分/]/."))
         .arg(Arg::with_name("subtract_files").multiple(true)
              .number_of_values(1)
              .short("-s")
@@ -28,7 +32,12 @@ fn main() {
              .help("Specify files to merge."))
         .get_matches();
 
+    let retain_okuri_entries = matches.is_present("retain_okuri_entries");
+
     let comment_re = Regex::new(r"^\s*;").unwrap();
+    let part_re = Regex::new(r"\[[^]]*\]|[^/]+").unwrap();
+
+    let mut entries : HashMap<String, Vec<String>> = HashMap::new();
 
     let merge_filenames = matches.values_of("files").map(|a| a.collect())
         .unwrap_or_else(|| vec!["-"]);
@@ -40,14 +49,78 @@ fn main() {
         let file = BufReader::new(file);
         let s = read_all_encoded(file);
         for line in s.lines() {
+            let line = line.trim();
             if comment_re.is_match(line) {
                 continue;
             }
-            eprintln!("{:?}", line);
+            let spc = line.find(" ").unwrap_or(line.len());
+            let hira = &line[..spc];
+            let candidates = &line[spc+1..];
+            let v = entries.entry(hira.to_string()).or_insert(vec![]);
+            for part in part_re.find_iter(candidates) {
+                let part = part.as_str();
+                let part = if part.starts_with("[") && part.ends_with("]") {
+                    if retain_okuri_entries {
+                        &part[1..part.len()-1]
+                    } else {
+                        continue;
+                    }
+                } else {
+                    part
+                };
+                if !v.iter().any(|x| x == part) {
+                    v.push(part.to_string());
+                }
+            }
+            // eprintln!("{:?}", (hira, candidates));
+            // for part in part_re.find_iter(candidates) {
+            //     // eprintln!("{:?}", part);
+            // }
         }
     }
     for &filename in &subtract_filenames {
-        println!("{:?}", filename);
+        eprintln!("Loading {}...", filename);
+        let file = File::open(filename).unwrap();
+        let file = BufReader::new(file);
+        let s = read_all_encoded(file);
+        for line in s.lines() {
+            let line = line.trim();
+            if comment_re.is_match(line) {
+                continue;
+            }
+            let spc = line.find(" ").unwrap_or(line.len());
+            let hira = &line[..spc];
+            let candidates = &line[spc+1..];
+            let v = if let Some(v) = entries.get_mut(hira) {
+                v
+            } else {
+                continue;
+            };
+            for part in part_re.find_iter(candidates) {
+                let part = part.as_str();
+                let part = if part.starts_with("[") && part.ends_with("]") {
+                    &part[1..part.len()-1]
+                } else {
+                    part
+                };
+                v.retain(|x| x != part);
+            }
+        }
+    }
+    eprintln!("Sorting...");
+    let mut entries : Vec<_> = entries.drain().collect();
+    entries.retain(|&(_, ref v)| !v.is_empty());
+    entries.sort();
+    for &(ref hira, ref v) in &entries {
+        print!("{} /", hira);
+        for part in v {
+            if part.find("/").is_some() {
+                print!("[{}]/", part);
+            } else {
+                print!("{}/", part);
+            }
+        }
+        println!("");
     }
 }
 
